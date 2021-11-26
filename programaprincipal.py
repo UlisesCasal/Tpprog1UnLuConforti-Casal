4a3feaaee62c8fdc396d886ef685589411427089
UlisesCasal <83846014+UlisesCasal@users.noreply.github.com>
2021-11-26 19:46:58 -0300

    Agregue la carga de datos
    Agregue una función que pasa los datos del archivo "cuentas.txt" a un vector de registros que contiene sus respectivos campos.

---
+++ programaprincipal.py
@@ -0,0 +1,54 @@
+#LIBRERIAS USADAS:
+from pyrecord import Record
+
+def cargadatos():
+
+    #ESTA FUNCION BUSCARA CARGAR LOS DATOS QUE SE ENCUENTRAN EN EL ARCHIVO A UN VECTOR DE REGISTROS
+    #CADA CUENTA TENDRA UN REGISTRO; COMPUESTO POR: (NRO CUENTA, APELLIDO, NOMBRE, DNI, TIPO DE CUENTA, SALDO, ESTADO)
+    #TAMBIEN DEVUELVE EL NUMERO DE CUENTAS HABILES CARGADAS EN EL VECTOR_CUENTAS
+
+    archivocuentas = open("cuentas.txt", "r")
+
+    #Creo el registro que contendra las cuentas:
+    cuenta = Record.create_type("cuenta", "numero", "apellido", "nombre", "dni", "tipocuenta", "saldo", "estado", 
+                numero = 0, apellido = " ", nombre = " ", dni = " ", tipocuenta = 0, saldo = 0.0, estado = " ")
+    
+    #creo el vector de cuentas sobredimensionado:
+    vector_cuentas = [cuenta]* 1000
+    vector_cuentas[0] = cuenta()
+
+    #CARGO LOS DATOS AL VECTOR: 
+
+    #Leo una linea para entrar al ciclo e inicializo el indice:
+    indice = 0
+    linea = archivocuentas.readline().strip().split(",")
+
+    while len(linea) == 7:
+
+        vector_cuentas[indice] = cuenta()
+        vector_cuentas[indice].numero = int(linea[0])
+        vector_cuentas[indice].apellido = linea[1]
+        vector_cuentas[indice].nombre = linea[2]
+        vector_cuentas[indice].dni = linea[3]
+        vector_cuentas[indice].tipocuenta = int(linea[4])
+        vector_cuentas[indice].saldo = float(linea[5])
+        vector_cuentas[indice].estado = bool(linea[6])
+
+        #Leo otra linea e incremento indice:
+        linea = archivocuentas.readline().strip().split(",")
+        indice += 1
+    
+    
+    
+    #El indice me dara la cantidad de cuentas que tengo cargadas en el vector:
+    cuentashabiles = indice
+
+    #Cierro el archivo:
+    archivocuentas.close()
+    
+
+    #RETORNO LAS CUENTAS, Y LA CANTIDAD DE CUENTAS HABILES:
+
+    return vector_cuentas, cuentashabiles
+
+cargadatos()
