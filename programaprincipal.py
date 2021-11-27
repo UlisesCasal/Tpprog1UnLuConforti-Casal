2361d9826043cef1d1b22f1d48ee4c194d5d86fc
UlisesCasal <83846014+UlisesCasal@users.noreply.github.com>
2021-11-27 19:31:15 -0300

    Agregue la consulta de saldo(p1)
    Agregue la consulta de saldo de cuentas (punto 1).

--- programaprincipal.py
+++ programaprincipal.py
@@ -1,5 +1,6 @@
 #LIBRERIAS USADAS:
 from pyrecord import Record
+import os
 
 def cargadatos():
 
@@ -51,4 +52,29 @@ def cargadatos():
 
     return vector_cuentas, cuentashabiles
 
-cargadatos()
+def consultacuenta(vector_cuentas, cuentashabiles):
+
+    #ESTA FUNCION AL LLAMARLA ME PERMITE CONSULTAR EL SALDO DE LA CUENTA DESEADA(INGRESANDOSE EL NUMERO DE LA MISMA POR TECLADO)
+
+    #PIDO NUMERO DE CUENTA QUE SE DESEA CONSULTAR:
+    cuenta = int(input(f"Ingrese el numero de la cuenta que desea consultar(entre 1.000 y {cuentashabiles+1000}): "))
+    direccion = cuenta - 1000   #LO DECREMENTO EN 1.000 PARA OBTENER LA POSICION DE LA CUENTA EN EL VECTOR_CUENTAS.
+
+   #BUSCO LA CUENTA EN EL VECTOR DE CUENTAS: 
+    saldo = vector_cuentas[direccion].saldo
+
+    #Limpio pantalla:
+    os.system("cls")
+
+    #INFORMO POR PANTALLA:
+    print(" "*20, f"CUENTA NUMERO: {cuenta}")
+    print(" "*20, f"SALDO: {round(saldo, 2)}") #Uso round para que me muestre solo dos decimales luego de la coma. 
+
+    #Muestro espacios en blanco para bajar el simbolo del sistema:
+    print()
+    print()
+    print()
+
+    return 
+
+
